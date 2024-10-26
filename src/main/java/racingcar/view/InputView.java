3b928f12f51b7ad5fb1a4e24673ff39e77632e9a
yedamhy <yeadalm04@gmail.com>
2024-10-27 01:36:03 +0900

    ✨ feat(racingcar.view): 입력값 검증 로직 추가
    - 자동차 이름의 길이를 5자 이하로 제한하는 validateCarsName 메서드 추가
    - 시도 횟수가 0 이상 9 이하인지 확인하는 validateTryNumber 메서드 추가
    - 숫자 입력 오류에 대한 예외 처리 추가 (IllegalArgumentException 사용)

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -11,12 +11,32 @@ public class InputView {
     public List<Car> getCars(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String[] carNames = Console.readLine().split(",");
-
+        validateCarsName(carNames);
         return Arrays.stream(carNames).map(Car::new).toList();
     }
 
     public int getTryNumber(){
         System.out.println("시도할 횟수는 몇 회인가요?");
-        return Integer.parseInt(Console.readLine());
+        try{
+            int tryNumber = Integer.parseInt(Console.readLine());
+            validateTryNumber(tryNumber);
+            return tryNumber;
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException("유효한 숫자를 입력해주세요.");
+        }
+    }
+
+    private void validateCarsName(String[] carNames){
+        for(String carName : carNames){
+            if(carName.length() > 5){
+                throw new IllegalArgumentException("각 자동차의 이름은 5글자 이하로 지어주세요.");
+            }
+        }
+    }
+
+    private void validateTryNumber(int tryNumber){
+        if(tryNumber < 0 || tryNumber > 10){
+            throw new IllegalArgumentException("시도 횟수는 0이상 9이하로 설정해주세요.");
+        }
     }
 }
